8a41ef77f66d4fae9979fd0ed2836c6a1e4e1e8d
legolas-zeng <814382690@qq.com>
2019-10-28 11:31:11 +0800

    最简单的http服务程序

--- src/code/k8s/main.go
+++ src/code/k8s/main.go
@@ -2,11 +2,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"log"
 )
 func main() {
+	log.Print("启动服务...")
 	http.HandleFunc("/home", func(w http.ResponseWriter, _ *http.Request) {
 		fmt.Fprint(w, "Hello! Your request was processed.")
 	},
 	)
-	http.ListenAndServe(":8000", nil)
+	log.Print("服务启动完成.....")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
+
+
+//打开 http://127.0.0.1:8000/home
