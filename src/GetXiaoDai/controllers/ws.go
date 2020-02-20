36cb6553edf3288499911e6fec69cd5970151f58
legolas-zeng <814382690@qq.com>
2020-02-20 17:40:51 +0800

    新增异步处理表格和websocket传输数据

--- src/GetXiaoDai/controllers/ws.go
+++ src/GetXiaoDai/controllers/ws.go
@@ -2,18 +2,15 @@ package controllers
 
 import (
 	"log"
-
-	"GetXiaoDai/models"
 	"time"
-
 	_ "fmt"
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/toolbox"
 	"github.com/gorilla/websocket"
 	"github.com/tealeg/xlsx"
 	"fmt"
 	"os"
 	"io"
+	"github.com/hpcloud/tail"
 )
 
 type WebSocketController struct {
@@ -26,34 +23,49 @@ var (
 )
 
 func (c *WebSocketController) Get() {
-
 	ws, err := upgrader.Upgrade(c.Ctx.ResponseWriter, c.Ctx.Request, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	clients[ws] = true
+	tailTask()
+}
 
-	timeTask()
+func tailTask() {
+	fmt.Println("启动websocket！！！")
+	go handleexcle()
+	go tailMsg()
 
 }
 
-func timeTask() {
+func tailMsg(){
+	filename := "C:\\Users\\Administrator\\Desktop\\test.log"
 
-	timeStr := "0/3 * * * * *" //每隔3秒执行
-	//toolbox 定时任务
-	t1 := toolbox.NewTask("timeTask", timeStr, func() error {
-
-		//todo do what you want
-		msg := models.SocketMessage{SocketMessage: "这是向页面发送的数据 " + time.Now().Format("2006-01-02 15:04:05")}
-		broadcast <- msg
-
-		return nil
+	tails, err := tail.TailFile(filename, tail.Config{
+		ReOpen: true,
+		Follow: true,
+		// Location:  &tail.SeekInfo{Offset: 0, Whence: 2},
+		MustExist: false,
+		Poll:      true,
 	})
+	if err != nil {
+		fmt.Println("tail file err:", err)
+		return
+	}
 
-	toolbox.AddTask("tk1", t1)
-	toolbox.StartTask()
-	handleexcle()
+	var msg *tail.Line
+	var ok bool
+	for {
+		msg, ok = <-tails.Lines
+		if !ok {
+			fmt.Printf("tail file close reopen, filename:%s\n", tails.Filename)
+			time.Sleep(100 * time.Millisecond)
+			continue
+		}
+		broadcast <- msg.Text
+	}
 }
+
 
 func handleexcle(){
 	// 打开文件
@@ -64,22 +76,25 @@ func handleexcle(){
 	}
 	sheet := xlFile.Sheets[0]
 	fmt.Println("工作表名: ", sheet.Name)
+	t1 := time.Now()
 	for _, row := range sheet.Rows[1:] {
 		number := row.Cells[0]
 		filename := row.Cells[7]
-		//name := row.Cells[2]
-		//path := row.Cells[8]
-		fullname := fmt.Sprintf("%s-%s", number, filename)
-		fmt.Println(fullname)
-		log2fileAndStdout(fullname)
-		//for _, cell := range row.Cells {
-		//    fmt.Println(cell)
-		//}
-		fmt.Print("\n")
+		fullname := fmt.Sprintf("C:\\Users\\Administrator\\Desktop\\%s-%s", number, filename)
+		srcpath := fmt.Sprintf("%s",row.Cells[8])
+
+		//fmt.Println(srcpath,fullname)
+		time.Sleep(2*time.Second)
+		_ , err := copy(srcpath,fullname)
+		if err == nil {
+			log2fileAndStdout(fmt.Sprintf("success-----%s的%s：%s拷贝完成",row.Cells[2],row.Cells[5],row.Cells[7]))
+		}else {
+			log2fileAndStdout(fmt.Sprintf("fail-----%s的%s：%s拷贝完成",row.Cells[2],row.Cells[5],row.Cells[7]))
+		}
 	}
-	fmt.Println("\n读取成功")
-	//func_log2file()
-	//func_log2fileAndStdout()
+	log2fileAndStdout(fmt.Sprintf("------全部完成！------"))
+	elapsed := time.Since(t1)
+	log2fileAndStdout(fmt.Sprintf("------共计%d行,总共用时%s！------",len(sheet.Rows),elapsed))
 
 }
 
@@ -100,3 +115,30 @@ func log2fileAndStdout(msg string) {
 	// 使用新的log对象，写入日志内容
 	logger.Println(msg)
 }
+
+
+func copy(src string, dst string) (int64, error) {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return 0, err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer destination.Close()
+	nBytes, err := io.Copy(destination, source)
+	return nBytes, err
+}
+
