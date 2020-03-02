972bd03c33ba385ce75207d029aaf2679c770a9d
legolas-zeng <814382690@qq.com>
2020-03-02 17:00:55 +0800

    接口功能的demo

---
+++ src/code/随手记/接口/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+// 接口可以理解为一些行为的集合。
+
+type TypeCalculator interface {
+    // 定义一个待实现的方法TypeCal()
+    TypeCal() string
+}
+
+type Worker struct {
+    Type int
+    Name string
+}
+
+type Student struct {
+    Name string
+}
+
+func (w Worker) TypeCal() string {
+    if w.Type == 0 {
+        return w.Name +"是蓝翔毕业的员工"
+    } else {
+        return w.Name+"不是蓝翔毕业的员工"
+    }
+}
+
+func (s Student) TypeCal() string  {
+    return s.Name + "还在蓝翔学挖掘机炒菜"
+}
+
+func main() {
+    // 创建两个struct结构体的实例
+    worker := Worker{Type:0, Name:"小华"}
+    student := Student{Name:"小明"}
+    // 把这两个实例放同一个TypeCalculator的切片中
+    workers := []TypeCalculator{worker, student}
+    // 遍历这个切片，并调用切片中的函数打印结果
+    for _, v := range workers {
+        fmt.Println(v.TypeCal())
+    }
+}
