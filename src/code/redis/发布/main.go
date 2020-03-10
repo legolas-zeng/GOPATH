6e0f1c57f5b3eb354b3cb01d1f9d869ad76b17ee
legolas-zeng <814382690@qq.com>
2020-03-10 14:50:42 +0800

    发布map信息

--- src/code/redis/发布/main.go
+++ src/code/redis/发布/main.go
@@ -4,8 +4,12 @@ import (
     "fmt"
     "github.com/gomodule/redigo/redis"
     "sync"
+    "k8s.io/apimachinery/pkg/util/json"
 )
 
+var(
+    cmdinfo = make(map[string]string)
+)
 func NewRedisClient() (conn redis.Conn, err error) {
     host := "127.0.0.1"
     port := "6379"
@@ -23,7 +27,11 @@ func Publish()  {
         Name *string
         Age *int
     }
-    _, err = conn.Do("Publish", "order-create", "1111111111111")
+    cmdinfo["2"] = "ping 192.168.3.5 -l 32000"
+    value,_ := json.Marshal(cmdinfo)
+    fmt.Println(value)
+    fmt.Printf("%T",value)
+    _, err = conn.Do("Publish", "order-create", value)
     if err != nil {
         fmt.Println("发布错误", err)
         return
