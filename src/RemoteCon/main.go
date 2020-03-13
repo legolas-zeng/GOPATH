7ae2a1ae10c3ef87418457fbfc2b4db93abd6c1c
legolas-zeng <814382690@qq.com>
2020-03-13 18:24:25 +0800

    配置数据库

--- src/RemoteCon/main.go
+++ src/RemoteCon/main.go
@@ -3,9 +3,32 @@ package main
 import (
 	_ "RemoteCon/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"RemoteCon/models"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 )
 
+func init() {
+	orm.Debug = true
+	orm.RegisterDataBase("default", "mysql", "zwa:qq1005521@tcp(192.168.3.5:3306)/pcinfo?charset=utf8", 30)
+	orm.RegisterModel(
+		new(models.Pcinfo),
+	)
+	err := orm.RunSyncdb("default", false, true)
+	if err != nil {
+		fmt.Println("数据库创建失败!!")
+		fmt.Println(err)
+	} else {
+		fmt.Printf("数据库初始化已完成！！")
+	}
+
+}
+
 func main() {
+	beego.SetStaticPath("/images","images")
+	beego.SetStaticPath("/css","css")
+	beego.SetStaticPath("/js","js")
 	beego.Run()
 }
 
