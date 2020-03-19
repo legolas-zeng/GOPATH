7717791ca83bf671275ce5885b7e59ce3093f120
legolas-zeng <814382690@qq.com>
2020-03-19 11:02:37 +0800

    想让goroutine订阅redis，结果重复读取，有问题

--- src/RemoteCon/controllers/remote.go
+++ src/RemoteCon/controllers/remote.go
@@ -3,12 +3,18 @@ package controllers
 import (
     "github.com/astaxie/beego"
     "RemoteCon/models"
+    "k8s.io/apimachinery/pkg/util/json"
     "fmt"
+    "github.com/go-redis/redis"
 )
 
 type RemoteController struct {
     beego.Controller
 }
+
+var (
+    cmdinfo=make(map[string]string)
+)
 
 func (this *RemoteController) GetPcinfo(){
     beego.ReadFromRequest(&this.Controller)
@@ -40,14 +46,57 @@ func (this *RemoteController) Remote(){
 
 func (this *RemoteController) ApiRemote(){
     ip := this.GetString("ip_list")
-    cmd := this.GetString("cmd")
+    cmds := this.GetString("cmd")
     argv1 := this.GetString("argv1")
-    fmt.Println(ip,cmd,argv1)
+    cmdinfo[ip]=argv1
+    fmt.Println("+++++++",cmdinfo)
     resp := make(map[string]interface{})
-    resp["status"] = 0
-    resp["msg"] = "刷新数据成功"
-    this.Data["json"] = resp
+    if cmds=="shell"{
+        fmt.Println("往redis消息队列发送信息：",cmdinfo)
+        conn, _ := NewRedisClient()
+        value,_ := json.Marshal(cmdinfo)
+        _, err := conn.Do("Publish", "order-create", value)
+        if err != nil{
+            resp["status"] = 0
+            resp["msg"] = "刷新数据成功"
+            this.Data["json"] = resp
+        }else {
+            resp["status"] = 1
+            resp["msg"] = "刷新数据失败"
+            this.Data["json"] = resp
+        }
+    }
+    go subscribeReq()
     this.ServeJSON()
 }
+
+
+// 订阅返回的信息
+func subscribeReq(){
+    client := redis.NewClient(&redis.Options{
+        Addr:     "127.0.0.1:6379", // redis地址
+        Password: "", // redis密码，没有则留空
+        DB:       0,  // 默认数据库，默认是0
+    })
+    sub := client.Subscribe("result")
+    fmt.Println(sub.Receive())
+    for msg := range sub.Channel(){
+        fmt.Println(msg.Payload)
+
+    }
+    return
+}
+
+
+
+//func sendCmdToRedis(iplist map[int]string,shell string) error {
+//    conn, _ := NewRedisClient()
+//
+//    value,_ := json.Marshal(cmdinfo)
+//    fmt.Println(value)
+//    fmt.Printf("%T",value)
+//    _, err := conn.Do("Publish", "order-create", value)
+//    return err
+//}
 
 
