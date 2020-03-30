aa4e2369ce1a7167e04caec177eab6163734bf00
legolas-zeng <814382690@qq.com>
2020-03-30 17:03:15 +0800

    断线重连

--- src/code/socket-zinx/客户端/client.go
+++ src/code/socket-zinx/客户端/client.go
@@ -2,10 +2,11 @@ package main
 
 import (
     "fmt"
-    "io"
     "net"
     "time"
+    "errors"
     "github.com/aceld/zinx/znet"
+    "io"
 )
 
 /*
@@ -13,55 +14,92 @@ import (
  */
 func main() {
 
-    fmt.Println("Client Test ... start")
-    //3秒之后发起测试请求，给服务端开启服务的机会
-    time.Sleep(3 * time.Second)
-
-    conn,err := net.Dial("tcp", "192.168.10.3:8999")
-    if err != nil {
-        fmt.Println("client start err, exit!")
-        return
-    }
+    //fmt.Println("Client Test ... start")
+    ////3秒之后发起测试请求，给服务端开启服务的机会
+    //time.Sleep(3 * time.Second)
+    service:="192.168.10.3:8999"
+    tcpAddr,_:=net.ResolveTCPAddr("tcp4",service)
+    //conn, err := net.Dial("tcp", "192.168.10.3:8999")
+    //if err != nil {
+    //    fmt.Println("客户端启动失败!")
+    //    return
+    //}
 
     for {
         //发封包message消息
-        dp := znet.NewDataPack()
-        msg, _ := dp.Pack(znet.NewMsgPackage(0,[]byte("Zinx Client Test Message")))
-        _, err := conn.Write(msg)
-        if err !=nil {
-            fmt.Println("write error err ", err)
-            return
-        }
-
-        //先读出流中的head部分
-        headData := make([]byte, dp.GetHeadLen())
-        _, err = io.ReadFull(conn, headData) //ReadFull 会把msg填充满为止
+        conn,err:=net.DialTCP("tcp",nil,tcpAddr)
         if err != nil {
-            fmt.Println("read head error")
-            break
-        }
-        //将headData字节流 拆包到msg中
-        msgHead, err := dp.Unpack(headData)
-        if err != nil {
-            fmt.Println("server unpack err:", err)
-            return
+            fmt.Printf("未连接服务端:%s \n", err)
+        } else {
+            fmt.Printf("连接服务端:%s \n", conn.RemoteAddr().String())
+            defer conn.Close()
+            doWork(conn)
         }
 
-        if msgHead.GetDataLen() > 0 {
-            //msg 是有data数据的，需要再次读取data数据
-            msg := msgHead.(*znet.Message)
-            msg.Data = make([]byte, msg.GetDataLen())
+        time.Sleep(1 * time.Second)
+    }
+}
 
-            //根据dataLen从io中读取字节流
-            _, err := io.ReadFull(conn, msg.Data)
-            if err != nil {
-                fmt.Println("server unpack data err:", err)
-                return
+//解决断线重连问题
+func doWork(conn net.Conn) error {
+    ch := make(chan int, 100)
+    ticker := time.NewTicker(time.Second)
+    defer ticker.Stop()
+    for {
+        select {
+        case stat := <-ch:
+            if stat == 2 {
+                return errors.New("None Msg")
             }
+        case <-ticker.C:
+            ch <- 1
+            go ClientMsgHandler(conn, ch)
+        case <-time.After(time.Second * 10):
+            defer conn.Close()
+            fmt.Println("超时断线....")
+        }
+    }
+    return nil
+}
 
-            fmt.Println("==> Recv Msg: ID=", msg.Id, ", len=", msg.DataLen, ", data=", string(msg.Data))
+func ClientMsgHandler(conn net.Conn, ch chan int) {
+    <-ch
+    //获取当前时间
+    msg := "❤❤❤❤❤❤"
+    go SendMsg(conn, msg)
+    go ReadMsg(conn)
+}
+
+func SendMsg(conn net.Conn, msg string) {
+    dp := znet.NewDataPack()
+    msgs, _ := dp.Pack(znet.NewMsgPackage(0, []byte(msg)))
+    conn.Write(msgs)
+}
+
+//接收服务端发来的消息
+func ReadMsg(conn net.Conn) {
+    dp := znet.NewDataPack()
+    headData := make([]byte, dp.GetHeadLen())
+    io.ReadFull(conn, headData) //ReadFull 会把msg填充满为止
+    msgHead, err := dp.Unpack(headData)
+    if err != nil {
+        fmt.Println("server unpack err:", err)
+        return
+    }
+
+    if msgHead.GetDataLen() > 0 {
+        //msg 是有data数据的，需要再次读取data数据
+        msg := msgHead.(*znet.Message)
+        msg.Data = make([]byte, msg.GetDataLen())
+
+        //根据dataLen从io中读取字节流
+        _, err := io.ReadFull(conn, msg.Data)
+        if err != nil {
+            fmt.Println("server unpack data err:", err)
+            return
         }
 
-        time.Sleep(1*time.Second)
+        fmt.Println("==> Recv Msg: ID=", msg.Id, ", len=", msg.DataLen, ", data=", string(msg.Data))
+
     }
 }
